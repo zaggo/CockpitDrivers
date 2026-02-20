872fb0380f342c2dd07697ac3ef3bfb4911cdee1
Eberhard Rensch <mail@pleasantsoftware>
2026-02-20 10:27:35 +0100

    Adjust for building on 328 processors

--- MotionActor/src/Configuration.h
+++ MotionActor/src/Configuration.h
@@ -11,9 +11,14 @@ const byte kActorAddress = 129; // Default Packet Serial address for Kangaroo is
 const uint32_t kKangarooBaudRate = 19200; // Default baud rate for Kangaroo. Adjust if needed.
 
 const uint8_t kCanIntPin = 2;
+#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
 const uint8_t kCanCSPin = 53;
+#else   
+const uint8_t kCanCSPin = 10;
+#endif
 
-const uint8_t kCANAlarmPin = 49; // LED on if CAN error/not initialized
+const uint8_t kRedLEDPin = 4; // LED on if CAN error/not initialized
+const uint8_t kGreenLEDPin = 3; // LED 
 
 // Exakte ID-Matches (alle 11 Bits relevant)
 const uint32_t MASK_EXACT = 0x07FF0000;
--- MotionActor/src/DebugLog.h
+++ MotionActor/src/DebugLog.h
@@ -3,14 +3,25 @@
 
 #define DEBUGLOG_ENABLE 0
 
-#if DEBUGLOG_ENABLE
-#define DEBUGLOG_INIT(x) Serial1.begin(x)
-#define DEBUGLOG_PRINT(x) Serial1.print(x)
-#define DEBUGLOG_PRINTLN(x) Serial1.println(x)
+// Prozessor erkennen
+#if defined(__AVR_ATmega328P__)
+    #include <SoftwareSerial.h>
+    static SoftwareSerial DebugSoftSerial(8, 9); // RX, TX 
+    #define DEBUGLOG_PORT DebugSoftSerial
+#elif defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+    #define DEBUGLOG_PORT Serial1
 #else
-#define DEBUGLOG_INIT(x)
-#define DEBUGLOG_PRINT(x)
-#define DEBUGLOG_PRINTLN(x)
+    #define DEBUGLOG_PORT Serial
+#endif
+
+#if DEBUGLOG_ENABLE
+    #define DEBUGLOG_INIT(x)    DEBUGLOG_PORT.begin(x)
+    #define DEBUGLOG_PRINT(x)   DEBUGLOG_PORT.print(x)
+    #define DEBUGLOG_PRINTLN(x) DEBUGLOG_PORT.println(x)
+#else
+    #define DEBUGLOG_INIT(x)
+    #define DEBUGLOG_PRINT(x)
+    #define DEBUGLOG_PRINTLN(x)
 #endif
 
 #endif // DEBUGLOG_H
